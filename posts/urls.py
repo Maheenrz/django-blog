4c5cf5cc1907e977b7a8646ca7a98aad02e5585d
Maheenrz <mahheen508@gmail.com>
2025-08-23 01:43:03 +0500

    Class 21 done

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<str:pk>', views.post, name='post')
+    path('post/<str:pk>', views.post, name='post'),
+    path('api/get/', views.get_data),
+    path('api/post/', views.post_data),
+    path('api/put/', views.put_data),
+    path('api/delete/', views.delete_data),
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
@@ -11,3 +13,26 @@ def post(request, pk):
     return render(request, 'posts.html', {'posts': posts})
 
 
+from django.http import JsonResponse
+
+# --- API Views ---
+
+
+@csrf_exempt
+def get_data(request):
+    return JsonResponse({"message": "This is GET request"})
+
+@csrf_exempt
+def post_data(request):
+    name = request.GET.get("name", "No Name Sent")
+    return JsonResponse({"message": f"This is POST request, name={name}"})
+
+@csrf_exempt
+def put_data(request):
+    name = request.GET.get("name", "No Name Sent")
+    return JsonResponse({"message": f"This is PUT request, name={name}"})
+
+@csrf_exempt
+def delete_data(request):
+    name = request.GET.get("name", "No Name Sent")
+    return JsonResponse({"message": f"This is DELETE request, name={name}"})
